1b70d0ae571926daa8735a9c3f2399e96df14689
erika helm <erikadphelm16@gmail.com>
2023-01-27 00:08:00 -0600

    getting functionality together!

--- scripts/app.js
+++ scripts/app.js
@@ -28,13 +28,13 @@ deleteActSuccess,
 const cards = document.querySelectorAll(".card");
 const signUpContainer = document.querySelector(".signUpContainer");
 const signInContainer = document.querySelector(".signInContainer");
-const indexListContainer = document.querySelector("#index-list-container");
+const indexListContainer = document.querySelector("#index-lists");
 const showListContainer = document.querySelector("#show-list-container");
 const createListContainer = document.querySelector("#create-list-container");
 const title = document.querySelector("#title")
 const act = document.querySelector("#act")
 const titleButton = document.querySelector("#title-button")
-const listContainer = document.querySelector("#list")
+const listContainer = document.querySelector("#index-list")
 const actContainer = document.querySelector("#create-act-container")
 // indexLists()
 // .then((res) => res.json())
@@ -43,31 +43,31 @@ const actContainer = document.querySelector("#create-act-container")
 // .catch(onError)
 
 
-signUpContainer.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const userData = {
-    credentials: {
-      userName: event.target["userName"].value,
-      password: event.target["password"].value,
-    },
-  }
-  signUp(userData).then(signUpSuccess).catch(onError);
-})
+// signUpContainer.addEventListener("submit", (event) => {
+//   event.preventDefault()
+//   const userData = {
+//     credentials: {
+//       userName: event.target["userName"].value,
+//       password: event.target["password"].value,
+//     },
+//   }
+//   signUp(userData).then(signUpSuccess).catch(onError);
+// })
 
-signInContainer.addEventListener("submit", (event) => {
-  event.preventDefault()
-  const userData = {
-    credentials: {
-      username: event.target["userName"].value,
-      password: event.target["password"].value,
-    },
-  }
-//   console.log(signIn)
-  signIn(userData)
-    .then((res) => res.json())
-    .then((res) => signInSuccess(res.token))
-    .catch(onError)
-})
+// signInContainer.addEventListener("submit", (event) => {
+//   event.preventDefault()
+//   const userData = {
+//     credentials: {
+//       username: event.target["userName"].value,
+//       password: event.target["password"].value,
+//     },
+//   }
+// //   console.log(signIn)
+//   signIn(userData)
+//     .then((res) => res.json())
+//     .then((res) => signInSuccess(res.token))
+//     .catch(onError)
+// })
 
 // cards.forEach((day) =>
 //   day.addEventListener("click", () => {
@@ -92,8 +92,18 @@ signInContainer.addEventListener("submit", (event) => {
 
 //created fucntion to add title to list
  function addTitleToList (){
-	listContainer.innerHTML=
-	"<li>" + title.value + "</li>"
+	listContainer.innerHTML=`
+  <li>${title.value}</li>
+  `
+  
+
+  
+  
+ 
+// document.querySelector("")
+
+
+
 	
   }
   
@@ -101,20 +111,33 @@ function addActToList(){
 	listContainer.innerHTML=
 	"<ul>" + act.value + "</ul>"
 
-}
+} 
 
-createListContainer.addEventListener("click", addTitleToList, addActToList, (event) => {
+createListContainer.addEventListener("click", addTitleToList, (event) => {
   event.preventDefault();
  
   const listData = {
     list: {
-      title: event.target["title"].value,
+      name: event.target["name"].value,
+      location: event.target["location"].value,
+      isComplete: event.target["is-complete"].checked
+
 	  
     },
   };
   createList(listData)
   .then(createListSuccess)
   .catch(onError)
-});
+})
 
 
+// createActContainer.addEventListener("submit", (e) => {
+//   e.preventDefault()
+
+//   const actData = {
+//     activity: {
+//       event.target[""]
+//     }
+    
+//   }
+// })
